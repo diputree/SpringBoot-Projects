90ff1f15797ecc92e5c37bcca58d5894ee7b82df
diputree <45426416+diputree@users.noreply.github.com>
2018-11-28 19:28:28 +0530

    Update SpringBootJpaApplication.java

--- SpringBootJpaApplication.java
+++ SpringBootJpaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootJpaApplication {
+	private static final String treeset = "Empty";
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJpaApplication.class, args);
